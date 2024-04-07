55716b65eb58bbd18574cb399b91174ae12a39dd
AbhinandanDaksh@gmail.com <abhinandandaksh@gmail.com>
2024-04-07 19:24:57 +0530

    frontend basecode

---
+++ navfinder/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className=' border-l-amber-950 bg-red-700'>
+    Hello word
+  </div>
+  );
+}
+
+export default App;
