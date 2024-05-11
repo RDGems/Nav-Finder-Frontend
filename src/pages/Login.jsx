d8272bbe0cfdfe23b5b6bfcde741ace6844ef20d
Abhay-Garg-Cdz <gargabhay1112@gmail.com>
2024-05-11 23:12:01 +0530

    Removed In with Google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import {
-  IconBrandGoogle,
-} from "@tabler/icons-react";
 
 
 const Login = () => {function handleSubmit() {}
@@ -59,20 +56,7 @@ return (
       >
         Login &rarr;
       </button>
-      <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-<div className="flex flex-col space-y-4">
-  <button
-    className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-    type="submit"
-  >
-    <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-    <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-      Login With Google
-    </span>
-    
-  </button>
-  </div>
+      
     </form>
   </div>
   </div>
